0a1cc2edfdb4922baba5eb410875f2618be97a06
mohammed <mohammedemail@gmail.com>
2024-07-14 20:24:59 +0300

    create user profil

--- app/Livewire/ProfilePage.php
+++ app/Livewire/ProfilePage.php
@@ -2,8 +2,12 @@
 
 namespace App\Livewire;
 
+use App\Models\Address;
+use App\Models\Order;
+use App\Models\User;
 use Livewire\Attributes\Title;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 
 #[Title('Profile - Epharmacy')]
@@ -13,6 +17,18 @@ class ProfilePage extends Component
 
     public function render()
     {
-        return view('livewire.profile-page');
+        $user_id=Auth::user()->id;
+        $user=User::where('id',$user_id)->first();
+
+        $orders = Order::where('client_id', $user_id)->with('order_delivery')->get();
+        
+        $addresses = Address::where('user_id', $user_id)->get();
+
+        return view('livewire.profile-page' ,[
+            
+            'user' => $user ,
+            'orders' => $orders,
+            'addresses'=> $addresses
+        ]);
     }
 }
